f392ba9764f1d24361c221658644d9b0d50ff389
Puttzer <53831562+Puttzer@users.noreply.github.com>
2019-11-04 16:31:38 +0100

    Paste over to this repository
    Having issues with commiting on previous repository, so now doing it here

---
+++ js/index.js
@@ -0,0 +1,72 @@
+// button element
+const form = document.querySelector("form");
+
+// make a function that allows us to close lightbox
+document.querySelector("#close-btn").addEventListener("click", () => {
+  document.querySelector("#light-box").style.display = "none";
+});
+//function that to be able to search for images through flickr api
+let searchfunction = e => {
+  e.preventDefault();
+  //get the value from input element
+  let keyword = document.querySelector("input").value;
+
+  //store value in varaiable "tags"
+  let tag = keyword;
+  // for each click, remove content that is already displayed
+  removeContent();
+  // run function to display images from Flickr API
+  fetchData(tag);
+};
+
+// a function that fetches data from the API.
+
+let fetchData = tag => {
+  // linked url to said API, that is stored in the variable "url"
+  //parameter "tags" is used in API as a defined area when keyword is added like say the word"space", the word itself is stored in the "tags" parameter and then placed in API which in return shows images related to that keyword
+  let url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=f0ba267d7ad25a32cb67fab3494fbc83&tags=${tag}&format=json&nojsoncallback=1`;
+
+  // fetch api which takes the varable "url" as its parameter
+  fetch(url)
+    //returns a promise that converts  the data to json format
+    .then(res => res.json())
+    //the return promise then convert parameter "res" into an obejct called "data"
+    .then(data => {
+      //loop through the data object in the order below
+      data.photos.photo.forEach(item => {
+        // create a local variable "url" that fetches each image using the url-link below
+        // each placeholder is used to build the image that is received from  the server
+        let url = `https://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}.jpg`;
+        // create a local variable called "output" which prints out an image tag.
+        //in source attribute, we then use the local variable "url".
+        let output = `
+        <img src = "${url}">
+      `;
+
+        //print out each image in div
+        document.getElementById("output").innerHTML += output;
+
+        //Lightbox
+        let imgArr = document.querySelectorAll("#output img");
+        imgArr.forEach(img =>
+          img.addEventListener("click", e => {
+            //Click on image creates the lightbox
+            let lightboxImage = document.querySelector("#light-box img");
+
+            // Takes the image url and puts it in the empty img src in html
+            let newSrc = e.target.getAttribute("src");
+            lightboxImage.setAttribute("src", newSrc);
+            document.querySelector("#light-box").style.display = "flex";
+          })
+        );
+      });
+    });
+};
+
+//function that removes content after each click
+let removeContent = () => {
+  document.getElementById("output").innerHTML = "";
+};
+
+//Run search function when user clicks on button or enter key
+form.addEventListener("click", searchfunction);
